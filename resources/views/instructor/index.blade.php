c9dd1811688ea7051607ceb9674fe18d9e187362
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2025-07-15 09:56:14 +0700

    update

--- resources/views/instructor/index.blade.php
+++ resources/views/instructor/index.blade.php
@@ -10,9 +10,9 @@
 <div class="page-content">
   
   @if ($status === '1')
-  <h4>Instructor Account IsA <span class="text-success">Active</span> </h4>
+  <h4>Instructor Account Is <span class="text-success">Active</span> </h4>
   @else   
-  <h4>Instructor Account IsA <span class="text-danger">InActive</span> </h4> 
+  <h4>Instructor Account Is <span class="text-danger">InActive</span> </h4> 
  <p class="text-danger"><b> Plz wait admin will check and approve your account</b> </p>
   @endif
 
