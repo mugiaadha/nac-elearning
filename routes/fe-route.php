49c30a5f7a824ffa1a0ed546e9c12a910b2ddc1d
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2025-07-21 16:17:00 +0700

    update

--- routes/fe-route.php
+++ routes/fe-route.php
@@ -4,10 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-Route::post('/session-login', function (Request $request) {
-    $credentials = $request->only('email', 'password');
-    if (Auth::attempt($credentials)) {
-        return response()->json(['message' => 'Login success']);
-    }
-    return response()->json(['message' => 'Invalid credentials'], 401);
+Route::group('/backend', function () {
+    Route::post('/login', function (Request $request) {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Login success']);
+        }
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    });
 });
