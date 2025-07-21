960071ad5489e8ffbd6a6511deb459a2e4e0f55b
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2025-07-21 16:21:12 +0700

    update

--- routes/fe-route.php
+++ routes/fe-route.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-Route::group(['prefix' => 'backend'], function () {
+Route::prefix('backend')->group(function () {
     Route::post('/login', function (Request $request) {
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
