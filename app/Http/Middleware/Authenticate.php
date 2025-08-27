af24ae8845093a74118db6cbf1786d846c4ace96
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2025-08-27 17:42:24 +0700

    update

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,12 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if ($request->expectsJson() || $request->is('api/*')) {
+            abort(response()->json([
+                'success' => false,
+                'message' => 'Mohon Login Terlebih Dahulu!',
+            ], 401));
+        }
+        return route('login');
     }
 }
