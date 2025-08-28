25f70cb443e97d0d62a01c079ca7502dcdf6a7fc
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2025-08-28 13:55:08 +0700

    update

--- config/logging.php
+++ config/logging.php
@@ -78,7 +78,7 @@ return [
             'url' => env('LOG_SLACK_WEBHOOK_URL', ''),
             'username' => 'Laravel Log',
             'emoji' => ':boom:',
-            'level' => env('LOG_LEVEL', 'debug'),
+            'level' => env('LOG_LEVEL', 'error'),
             'replace_placeholders' => true,
         ],
 
