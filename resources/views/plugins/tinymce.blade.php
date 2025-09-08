7eb70cf65192b402cd87f32ae38e1d2c3566813f
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2025-09-08 15:55:50 +0700

    update

--- resources/views/plugins/tinymce.blade.php
+++ resources/views/plugins/tinymce.blade.php
@@ -1,8 +1,14 @@
-<script src="https://cdn.tiny.cloud/1/vwjzqtca2m29ckujxypd6t88vv9wzuv8wdzpp2cjehwrv367/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+<!-- Place the first <script> tag in your HTML's <head> -->
+<script src="https://cdn.tiny.cloud/1/vwjzqtca2m29ckujxypd6t88vv9wzuv8wdzpp2cjehwrv367/tinymce/8/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
+
+<!-- Place the following <script> and <textarea> tags your HTML's <body> -->
 <script>
     tinymce.init({
-        selector: 'textarea#myeditorinstance', // Replace this CSS selector to match the placeholder element for TinyMCE
-        plugins: 'powerpaste advcode table lists checklist',
-        toolbar: 'undo redo | blocks| bold italic | bullist numlist checklist | code | table'
+        selector: 'textarea',
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
     });
 </script>
+<textarea>
+  Welcome to TinyMCE!
+</textarea>
